b6b8311025b62c1d4e9f661edfb7dc3b82889b2f
Yousef Javaherian <yousefjavaherian@gmail.com>
2022-12-29 23:04:15 +0330

    chaning path -> ws_path

--- entry.sh
+++ entry.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-export VARS='$PORT:$PASSWORD:$PATH:$HOST'
 /bin/sed -i "s/\$PORT/$PORT/g" /etc/shadowsocks-rust/config.json
 /bin/sed -i "s/\$PASSWORD/$PASSWORD/g" /etc/shadowsocks-rust/config.json
-/bin/sed -i "s/\$PATH/$PATH/g" /etc/shadowsocks-rust/config.json
+/bin/sed -i "s/\$WS_PATH/$WS_PATH/g" /etc/shadowsocks-rust/config.json
 /bin/sed -i "s/\$HOST/$HOST/g" /etc/shadowsocks-rust/config.json
 # envsubst $VARS < /etc/shadowsocks-rust/config.json.var > /etc/shadowsocks-rust/config.json
 exec "$@"
