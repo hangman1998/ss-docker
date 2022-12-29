339388a331a3a53d12c83f0e1e7f6eb44ca04f0e
Yousef Javaherian <yousefjavaherian@gmail.com>
2022-12-29 22:54:08 +0330

    testing /bin/sed

--- entry.sh
+++ entry.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export VARS='$PORT:$PASSWORD:$PATH:$HOST'
-sed -i "s/\$PORT/$PORT/g" /etc/shadowsocks-rust/config.json
-sed -i "s/\$PASSWORD/$PASSWORD/g" /etc/shadowsocks-rust/config.json
-sed -i "s/\$PATH/$PATH/g" /etc/shadowsocks-rust/config.json
-sed -i "s/\$HOST/$HOST/g" /etc/shadowsocks-rust/config.json
+/bin/sed -i "s/\$PORT/$PORT/g" /etc/shadowsocks-rust/config.json
+/bin/sed -i "s/\$PASSWORD/$PASSWORD/g" /etc/shadowsocks-rust/config.json
+/bin/sed -i "s/\$PATH/$PATH/g" /etc/shadowsocks-rust/config.json
+/bin/sed -i "s/\$HOST/$HOST/g" /etc/shadowsocks-rust/config.json
 # envsubst $VARS < /etc/shadowsocks-rust/config.json.var > /etc/shadowsocks-rust/config.json
 exec "$@"
