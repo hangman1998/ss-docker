50ed3a8eb22e7ea8ae73ff3b7ae0faf6a43e1110
Yousef Javaherian <yousefjavaherian@gmail.com>
2022-12-29 19:16:28 +0330

    using base CMD

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ COPY config.json  /etc/shadowsocks-rust/config.json.var
 COPY entry.sh /entry.sh
 RUN chmod +x /entry.sh
 ENTRYPOINT ["/entry.sh"]
+CMD [ "/usr/bin/ssservice", "server", "--log-without-time", "-c", "/etc/shadowsocks-rust/config.json" ]
--- entry.sh
+++ entry.sh
@@ -1,5 +1,4 @@
+#!/bin/sh
 export VARS='$PORT:$PASSWORD:$PATH:$HOST'
 envsubst $VARS < /etc/shadowsocks-rust/config.json.var > /etc/shadowsocks-rust/config.json
-exec ssserver -c /etc/shadowsocks-rust/config.json
-# exec /usr/bin/ssserver --log-without-time -c /etc/shadowsocks-rust/config.json
-# exec /usr/bin/v2ray -config /etc/v2ray/config.json
+exec "$@"
