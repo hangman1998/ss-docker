25f2330e7feb60755567b8fbc18d08ad69ee1257
Yousef Javaherian <yousefjavaherian@gmail.com>
2022-12-29 18:24:37 +0330

    changing exec command

--- entry.sh
+++ entry.sh
@@ -1,3 +1,4 @@
 export VARS='$PORT:$PASSWORD:$PATH:$HOST'
 envsubst $VARS < /etc/shadowsocks-rust/config.json.var > /etc/shadowsocks-rust/config.json
-exec /usr/bin/ssservice  server --log-without-time -c /etc/shadowsocks-rust/config.json
+exec /usr/bin/ssserver --log-without-time -c /etc/shadowsocks-rust/config.json
+# exec /usr/bin/v2ray -config /etc/v2ray/config.json
