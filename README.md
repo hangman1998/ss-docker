a5f0de4fdba83b04126cc72545c911d206cf6b19
Yousef <yousefjavaherian@gmail.com>
2023-05-03 17:37:33 +0330

    removed host env variable

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # shadowsocks docker
+
 a docker iamge based of [shadowsocks-rust](https://hub.docker.com/r/teddysun/shadowsocks-rust) using `v2ray` plugin.
 
 ## enviroment variables
+
 - `PORT`: server listening port
 - `WS_PATH`: the path used in plugin configuration (do not include `/` in the path)
-- `HOST`: the domain 
 - `PASSWORD`: shadowsocks password
-
--- entry.sh
+++ entry.sh
@@ -2,7 +2,5 @@
 /bin/sed -i "s/\$PORT/$PORT/g" /etc/shadowsocks-rust/config.json
 /bin/sed -i "s/\$PASSWORD/$PASSWORD/g" /etc/shadowsocks-rust/config.json
 /bin/sed -i "s/\$WS_PATH/$WS_PATH/g" /etc/shadowsocks-rust/config.json
-/bin/sed -i "s/\$HOST/$HOST/g" /etc/shadowsocks-rust/config.json
 cat /etc/shadowsocks-rust/config.json
-# envsubst $VARS < /etc/shadowsocks-rust/config.json.var > /etc/shadowsocks-rust/config.json
 exec "$@"
